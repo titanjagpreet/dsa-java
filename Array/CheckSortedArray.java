90238a68401ff8eb6615cd6d9708b827540a0044
titanjagpreet <titanjagpreetsingh@gmail.com>
2026-07-18 22:19:02 +0530

    check sorted array

---
+++ Array/CheckSortedArray.java
@@ -0,0 +1,25 @@
+public class CheckSortedArray {
+    public static void main(String args[]) {
+
+    }
+
+    public boolean check(int[] nums) {
+
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+
+            if (count > 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
