40f67cc6b37d5949361ecd3fc56ebc1e54d21483
titanjagpreet <titanjagpreetsingh@gmail.com>
2026-07-21 21:54:49 +0530

    find majority element in array

---
+++ Array/MajorityElement.java
@@ -0,0 +1,29 @@
+public class MajorityElement {
+    public static void main(String args[]) {
+        int[] arr = {2, 2, 1, 2, 3, 2, 2};
+
+        System.out.println(majorityElement(arr));
+    }
+
+    public static int majorityElement(int[] nums) {
+
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+
+}
